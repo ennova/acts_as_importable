38a483699ed8100f45c19d452f3d3bd1435d7806
Bhavin Javia <bjavia@gmail.com>
2010-12-31 16:44:29 +0530

    Fixed failing test due to missing UPLOADS_PATH

--- test/test_acts_as_importable/spec/controllers/products_controller_spec.rb
+++ test/test_acts_as_importable/spec/controllers/products_controller_spec.rb
@@ -54,6 +54,7 @@ describe ProductsController do
 
   private
   def create_test_file(controller_name, products)
+    FileUtils.mkdir_p(UPLOADS_PATH)
     filename = "#{UPLOADS_PATH}/#{controller_name}.csv"
     FasterCSV.open(filename, "w") do |csv|
       csv << ["name", "price"]
